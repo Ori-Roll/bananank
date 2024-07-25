3358a4bc4bfd5d4892c0fa6699bf970598a33884
Ori Roll <oriroll@gmail.com>
2024-07-25 16:02:51 +0300

    changes check

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send({ users: ['John', 'Betty', 'Hal'] });
+  res.send({ users: ['John', 'Betty', 'Hal', 'Mr. T'] });
 });
 
 module.exports = router;
